7b76c76de15958b24b22cecc79da98065c041c7e
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-01-07 23:12:35 -0500

    Deleted Storyboard Setup project to be built using UIKit Completed tutorial

--- ProcessImagesFromCameraTutorial1/MainViewController.swift
+++ ProcessImagesFromCameraTutorial1/MainViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import AVFoundation
 
-class MainViewController: UIViewController {
+
+class MainViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    private let captureSession = AVCaptureSession()
+    
+    private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let preview = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        preview.videoGravity = .resizeAspect
+        return preview
+    }()
+    
+    private func addPreviewLayer() {
+        self.view.layer.addSublayer(self.previewLayer)
+    }
+
+    private func addCameraInput() {
+        let captureDevice = AVCaptureDevice.default(for: .video)!
+        let cameraInput = try! AVCaptureDeviceInput(device: captureDevice)
+        captureSession.addInput(cameraInput)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         view.backgroundColor = .white
+        
+        
+        self.addCameraInput()
+        self.addPreviewLayer()
+        self.addVideoOutput()
+        self.captureSession.startRunning()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.previewLayer.frame = self.view.bounds
+    }
+    
+    private let videoOutput = AVCaptureVideoDataOutput()
+    
+    private func addVideoOutput() {
+        self.videoOutput.videoSettings = [
+            (kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)
+        ] as [String: Any]
+        self.videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "my.image.handling.queue"))
+        self.captureSession.addOutput(self.videoOutput)
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let frame = CMSampleBufferGetImageBuffer(sampleBuffer)
+        else {
+            debugPrint("Unable to get image from sample buffer")
+            return
+        }
+        print("Image frame received")
+        // process image here
+        
     }
 
 
